2351e041578cdc34ec716826c9b656eedd294bd4
sjKim <ksj0818@github.com>
2023-08-11 21:01:25 +0900

    그리드 슬라이드 추가

--- components/Slide.tsx
+++ components/Slide.tsx
@@ -2,7 +2,8 @@ import { Swiper } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import { Autoplay, Pagination, Navigation, Grid } from "swiper/modules";
+import { useEffect, useState } from "react";
 
 export default function Slide({ children }: { children: React.ReactNode }) {
   return (
@@ -20,6 +21,33 @@ export default function Slide({ children }: { children: React.ReactNode }) {
       modules={[Autoplay, Pagination, Navigation]}
       className="mySwiper"
       slidesPerView={1}
+    >
+      {children}
+    </Swiper>
+  );
+}
+
+export function GridSlide({ children }: { children: React.ReactNode }) {
+  const [viewSize, setViewSize] = useState(0);
+
+  useEffect(() => {
+    setViewSize(window.innerWidth);
+    console.log(viewSize);
+  }, [viewSize]);
+  return (
+    <Swiper
+      slidesPerView={3}
+      grid={{
+        rows: 1,
+      }}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+      spaceBetween={30}
+      navigation={true}
+      modules={[Grid, Navigation, Autoplay]}
+      className="mySwiper"
     >
       {children}
     </Swiper>
