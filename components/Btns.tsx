48f001b27ea402bb09aed63f5e0dd2681571d698
sjKim <ksj0818@github.com>
2023-08-11 20:59:07 +0900

    상품 버튼 생성

---
+++ components/Btns.tsx
@@ -0,0 +1,6 @@
+export function SpecialpriceBtn() {
+  return <button className="bg-rose-400 text-white rounded-md text-xs p-1 font-semibold mr-1">특가</button>;
+}
+export function DeliveryBtn() {
+  return <button className="bg-zinc-300 text-zinc-500 rounded-md text-xs p-1 font-semibold mr-1">무료배송</button>;
+}
