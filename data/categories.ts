a19372e7ef69040949beedb154d6adfeca35cd2f
sjKim <ksj0818@github.com>
2023-08-13 05:20:14 +0900

    카테고리, 제품 리스트 파일 구조화

---
+++ data/categories.ts
@@ -0,0 +1,62 @@
+export const CATEGORIES = [
+  {
+    name: "쇼핑하기",
+    url: "/product",
+    imgPath:
+      "https://image.ohou.se/i/bucketplace-v2-development/uploads/shortcut/home_feed_shortcut_sets/167262690767101882.png?w=256",
+  },
+  {
+    name: "100%당첨",
+    url: "/",
+    imgPath:
+      "https://image.ohou.se/i/bucketplace-v2-development/uploads/shortcut/home_feed_shortcut_sets/169111435186783648.png?w=256",
+  },
+  {
+    name: "내일의딜",
+    url: "/",
+    imgPath:
+      "https://image.ohou.se/i/bucketplace-v2-development/uploads/shortcut/home_feed_shortcut_sets/167198007152582471.png?w=256",
+  },
+  {
+    name: "내일의발견",
+    url: "/",
+    imgPath:
+      "https://image.ohou.se/i/bucketplace-v2-development/uploads/shortcut/home_feed_shortcut_sets/168661846720664430.png?w=256",
+  },
+  {
+    name: "장보기",
+    url: "/",
+    imgPath:
+      "https://image.ohou.se/i/bucketplace-v2-development/uploads/shortcut/home_feed_shortcut_sets/167198009533480922.png?w=256",
+  },
+  {
+    name: "집들이",
+    url: "/",
+    imgPath:
+      "https://image.ohou.se/i/bucketplace-v2-development/uploads/shortcut/home_feed_shortcut_sets/167198010271968429.png?w=256",
+  },
+  {
+    name: "취향의발견",
+    url: "/",
+    imgPath:
+      "https://image.ohou.se/i/bucketplace-v2-development/uploads/shortcut/home_feed_shortcut_sets/167505411234148010.png?w=256",
+  },
+  {
+    name: "빠른배송",
+    url: "/",
+    imgPath:
+      "https://image.ohou.se/i/bucketplace-v2-development/uploads/shortcut/home_feed_shortcut_sets/167198012024333402.png?w=256",
+  },
+  {
+    name: "쉬운이사",
+    url: "/",
+    imgPath:
+      "https://image.ohou.se/i/bucketplace-v2-development/uploads/shortcut/home_feed_shortcut_sets/167198021046015480.png?w=256",
+  },
+  {
+    name: "리모델링",
+    url: "/",
+    imgPath:
+      "https://image.ohou.se/i/bucketplace-v2-development/uploads/shortcut/home_feed_shortcut_sets/167198022841390557.png?w=256",
+  },
+];
---
+++ data/product.ts
@@ -0,0 +1,62 @@
+export const posts = [
+  {
+    id: "1",
+    brand: "맥심",
+    title: "[중복쿠폰] 모카/화이트골드 커피믹스 340티(170T*2)",
+    sale: "20%",
+    price: "45,500",
+    review: "203",
+    imgPath:
+      "https://image.ohou.se/i/bucketplace-v2-development/uploads/deals/169171606835618888.jpg?gif=1&w=720&h=720&c=c&webp=1",
+    specialpriceBTN: true,
+    deliveryBTN: true,
+  },
+  {
+    id: "2",
+    brand: "헬로우슬립",
+    title: "[단하루!추가인하+쿠폰] 부드러운 카스테라 항균 옥수수솜 간절기/사계절 차렵이불세트",
+    sale: "56%",
+    price: "34,900",
+    review: "23,201",
+    imgPath:
+      "https://image.ohou.se/i/bucketplace-v2-development/uploads/productions/167168822852557476.jpg?gif=1&w=850&h=850&c=c&webp=1",
+    specialpriceBTN: true,
+    deliveryBTN: false,
+  },
+  {
+    id: "3",
+    brand: "일리",
+    title: "[화이트 재고 입고]국내배송 Y3.3 커피머신 4color(화이트/블랙/레드/블루)",
+    sale: "61%",
+    price: "77,000",
+    review: "68",
+    imgPath:
+      "https://image.ohou.se/i/bucketplace-v2-development/uploads/productions/168791780292336854.jpg?gif=1&w=850&h=850&c=c&webp=1",
+    specialpriceBTN: true,
+    deliveryBTN: false,
+  },
+  {
+    id: "4",
+    brand: "슈퍼대디",
+    title: "[8/11일 딱하루! 선착순 21,900원]물티슈 미스터펭 블루 70매X20팩 (77g)",
+    sale: "48%",
+    price: "25,900",
+    review: "10",
+    imgPath:
+      "https://image.ohou.se/i/bucketplace-v2-development/uploads/productions/169113548786924944.jpg?gif=1&w=850&h=850&c=c&webp=1",
+    specialpriceBTN: true,
+    deliveryBTN: true,
+  },
+  {
+    id: "5",
+    brand: "LG생활건강",
+    title: "[15%+20%] 초특가! 테크/아우라/홈스타/자연퐁 초고농축 세탁/청소/주방세제 할인모음",
+    sale: "19%",
+    price: "11,900",
+    review: "4151",
+    imgPath:
+      "https://image.ohou.se/i/bucketplace-v2-development/uploads/deals/167817045428637941.jpg?gif=1&w=850&h=850&c=c&webp=1",
+    specialpriceBTN: true,
+    deliveryBTN: false,
+  },
+];
