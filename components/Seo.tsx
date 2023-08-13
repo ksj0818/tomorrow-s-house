50a47d600213d6a0b63e9872a525d00227269737
sjKim <ksj0818@github.com>
2023-08-14 07:20:20 +0900

    타이틀 옵션 수정

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -1,10 +1,13 @@
 import Head from "next/head";
 
 type Props = {
-  title: string;
+  title?: string;
   content?: string;
 };
-export default function Seo({ title, content = "감도높은 유저들의 일상 구경부터 제품 구매까지 한번에" }: Props) {
+export default function Seo({
+  title = "라이프스타일 슈퍼앱",
+  content = "감도높은 유저들의 일상 구경부터 제품 구매까지 한번에",
+}: Props) {
   return (
     <>
       <Head>
